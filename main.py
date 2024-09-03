5be50b49f8462ad0a9ee9dee4f404fc8805883f0
Anonymous <141341450+bpopivnyak@users.noreply.github.com>
2024-09-03 17:29:39 +0300

    kfkf

--- main.py
+++ main.py
@@ -6,12 +6,43 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.core.window import Window
 
 
-Window.size = (800,600)
+Window.size = (800, 600)
 
-class ButtoFF(App):
+
+
+class MainScreen(Screen):
+    def go_to_game_selection(self, *args):
+        self.manager.current = "game_selection"
+
+    def __init__(self, **kwargs):
+        super(MainScreen, self).__init__(**kwargs)
+        layout = BoxLayout(orientatoin="vertical")
+        self.add_widget(layout)
+
+        self.title = Label(text="Шиндовс", font_size="48sp", size_hint=(1, 0.8))
+        layout.add_widget(self.title)
+
+
+# Створити дві бутон
+        play_button = Button(text="стартуєм", seze_hint=(1, 0.2))
+        play_button.bind(on_press=self.go_to_game_selection)
+        layout.add_widget(play_button)
+
+
+class ClickerApp(App):
     def build(self):
-        layout = BoxLayout(orientation = "vertical")
-        button1 = Button(text="Баскетбол")
-        layout.add_widget(button1)
-        button1.bind(on_press=self.on_button1_press)
+        sm = ScreenManager()
+        sm.add_widget(MainScreen(name="main"))
+        return sm
+
+
+if __name__ == "__main__":
+    ClickerApp().run()
+
+
+
+
+
+
+
 
